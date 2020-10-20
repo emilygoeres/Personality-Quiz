218de1fb0fe51de06dd295ef53ffc71a96a67e58
Ann Guinee <annmguinee@gmail.com>
2020-10-20 15:50:04 -0700

    Added necessary click events, updated handlebars files

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -2,7 +2,7 @@ $("#history").on("click", event => {
     const selected = $(event.target);
     if(selected.data("next")==="retakeButton") {
         //window.location quiz with an id of selected.data("quizID")
-        //const id = $(this).data("quizID")
+        //const id = selected.data("quizID")
     }else if (selected.data("next")==="deleterButton") {
         /*
         const id = $(this).data("quizID")
