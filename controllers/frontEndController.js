286ee736f3a9d5b815b3d416bfc523ae32da42a4
Steven Bong <stevenseal99@yahoo.com>
2020-10-20 11:24:42 -0700

    Made boiler plate for the front-end. Need to add more details to this later

---
+++ controllers/frontEndController.js
@@ -0,0 +1,32 @@
+// const express = require("express");
+// const router = express.Router();
+// const db = require("../models");
+
+// Display the login page
+// router.get("/", function(req, res) {
+//     db
+//     res.render("index", hbsObject);
+// });
+  
+// Display the home page
+// router.get("/home/:id", function(req, res) {
+//     cat.all(function(data) {
+//       var hbsObject = {
+//         cats: data
+//       };
+//       console.log(hbsObject);
+//       res.render("index", hbsObject);
+//     });
+// });
+
+// Display the profile page
+// router.get("/profile/:id", function(req, res) {
+//     console.log(hbsObject);
+//     res.render("index", hbsObject);
+// });
+
+// Display the questions page
+// router.get("/question", function(req, res) {
+//     console.log(hbsObject);
+//     res.render("index", hbsObject);
+// });
