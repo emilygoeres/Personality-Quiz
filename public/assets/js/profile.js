9205012a25f0524a771a1110823751f3738e3cac
Ann Guinee <annmguinee@gmail.com>
2020-10-20 14:13:14 -0700

    Created profile js

---
+++ public/assets/js/profile.js
@@ -0,0 +1,18 @@
+$("#history").on("click", event => {
+    const selected = $(event.target);
+    if(selected.data("next")==="retakeButton") {
+        //window.location quiz with an id of selected.data("quizID")
+        //const id = $(this).data("quizID")
+    }else if (selected.data("next")==="deleterButton") {
+        /*
+        const id = $(this).data("quizID")
+        $.ajax("/api/users/quiz/" + id, {
+            type:'DELETE'
+        }).then( function() {
+            console.log("Deleted quiz from user " + id);
+            location.reload()
+        })
+        
+        */
+    }
+});
