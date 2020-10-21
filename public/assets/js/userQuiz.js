2c3f72004e302737139d2b0c303e508d6946a3a8
Ann Guinee <annmguinee@gmail.com>
2020-10-20 22:39:23 -0700

    HTML template for user creation

---
+++ public/assets/js/userQuiz.js
@@ -0,0 +1,9 @@
+//Post one big object into a create api route 
+
+//INSERT INTO Quizzes(quiz_name,quiz_category) values (qName, qCategory), get back PK quiz id
+
+//Henceforth, quiz id (primary key) = quizID
+
+//INSERT INTO personalities (pName, pArchetype, pDescription )
+
+//INSERT INTO questions  (questionText )
