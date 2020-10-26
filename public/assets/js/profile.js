98b2c4f9a53bf217e8b538fd173538c96a1c4fad
Ann Guinee <annmguinee@gmail.com>
2020-10-26 15:58:04 -0700

    added about modals

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -8,33 +8,46 @@ $.ajax({
 
 $("#history").on("click", event => {
     const selected = $(event.target);
-    if(selected.data("next")==="retakeButton") {
+    if (selected.data("next") === "retakeButton") {
         const id = selected.data("quizid");
-        location.href = "/quiz/"+id;
-    }else if (selected.data("next")==="deleterButton") {
-        
+        location.href = "/quiz/" + id;
+    } else if (selected.data("next") === "deleterButton") {
+
         const id = selected.data("quizid");
         $.ajax("/api/quizTaken/" + id, {
-            type:'DELETE'
-        }).then( function() {
+            type: 'DELETE'
+        }).then(function () {
             location.reload()
         })
-        
-        
+
+
     }
 });
 
-$(".modalStart").on("click", function(event){
-    $(".modal").toggleClass("is-active")
+$(".modalStart.archetypeDefinition").on("click", function (event) {
+    $(".modal#archetypeDefinition").toggleClass("is-active")
     const archetypeId = $(this).data("archid");
-    $("#archetypeName").text(archetypeDefinitions[archetypeId-1].archetype.toUpperCase());
-    $("#archetypeDescription").text(archetypeDefinitions[archetypeId-1].archetype_description);
+
+    $("#archetypeName").text(archetypeDefinitions[archetypeId - 1].archetype.toUpperCase());
+    $("#archetypeDescription").text(archetypeDefinitions[archetypeId - 1].archetype_description);
 })
 
-$(".modal-background").on("click", function(event){
-    $(".modal").toggleClass("is-active")
+$(".modal-background.aDef").on("click", function (event) {
+    $(".modal#archetypeDefinition").toggleClass("is-active")
 })
 
-$(".delete").on("click", function(event){
-    $(".modal").toggleClass("is-active")
+$(".delete.aDef").on("click", function (event) {
+    $(".modal#archetypeDefinition").toggleClass("is-active")
+})
+
+$(".modalStart.about").on("click", function(event){
+    $(".modal#about").toggleClass("is-active")
+})
+
+$(".modal-background.about").on("click", function(event){
+    $(".modal#about").toggleClass("is-active")
+})
+
+$(".delete.about").on("click", function(event){
+    $(".modal#about").toggleClass("is-active")
 })
